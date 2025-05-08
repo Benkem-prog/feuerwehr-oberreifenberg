9fbf33f3137233ea4f3d4fea872a42e48c32fdf6
Kempf <kempf@miebach.com>
2025-05-08 15:28:02 +0200

    Initial commit der modernen Feuerwehr-Website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'ff-gelb': '#E8EB3D',
+        'ff-dunkelgrau': '#4C515E',
+        'ff-hellgrau': '#D7D8DC',
+        'ff-rot': '#D62828'
+      }
+    }
+  },
+  plugins: []
+};
